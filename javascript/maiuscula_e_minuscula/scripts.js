d9b2c489deb94b5eef48400c8a3c8f6b6dffc0d5
Vagner Oliveira <vagner_oliveira85@hotmail.com>
2025-03-01 09:14:34 -0300

    Novas aulas
    Função construtora, length, replace.

---
+++ javascript/maiuscula_e_minuscula/scripts.js
@@ -0,0 +1,12 @@
+// Maiuscula e minuscula
+
+let message = "Estou estudando os fundamentos do JavaScript"
+
+console.log(message)
+
+// Exibir em maiúsculo
+
+console.log(message.toUpperCase())
+
+// Exibir em minúsculo
+console.log(message.toLowerCase())
---
+++ javascript/tamanho_string/scripts.js
@@ -0,0 +1,18 @@
+// Comprimento de uma string
+
+let message = "Estou estudando os fundamentos do JavaScript"
+
+// Comprimento de uma string
+console.log(message.length)
+
+let password = "12345"
+
+if (password.length < 6) {
+    console.log("A senha deve ter ao menos 6 caracteres")
+}
+
+// Quantos dígitos tem um número
+let value = 12345
+console.log(String(value).length)
+
+console.log(value.toString().length)
